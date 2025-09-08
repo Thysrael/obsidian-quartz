74a2ec982f3ded8a781a2482f93cb4d005ca29dc
Thysrael <thysrael@163.com>
2025-09-08 17:10:03 +0800

    2025-08-15

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Roam",
-    pageTitleSuffix: "Can you hear me?",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
