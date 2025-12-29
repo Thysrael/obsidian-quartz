34b44c72e04714d536e9db04b579838e30b16435
Thysrael <thysrael@163.com>
2025-12-29 15:52:32 +0800

    feat: add google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-KRSC8PN4XW",
     },
     locale: "en-US",
     baseUrl: "thysrael.github.io/obsidian-quartz/",
