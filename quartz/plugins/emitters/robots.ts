7977cff9aa14ffcec7a43944bf3e4990447d6bfd
Thysrael <thysrael@163.com>
2025-12-29 15:42:54 +0800

    feat: add robot.txt

---
+++ quartz/plugins/emitters/robots.ts
@@ -0,0 +1,21 @@
+import { FilePath, joinSegments } from "../../util/path"
+import { QuartzEmitterPlugin } from "../types"
+import fs from "fs"
+
+export const RobotsTxt: QuartzEmitterPlugin = () => ({
+  name: "RobotsTxt",
+  async emit({ argv, cfg }) {
+    const path = joinSegments(argv.output, "robots.txt")
+    const baseUrl = cfg.configuration.baseUrl ?? ""
+    const sitemapUrl = `https://${baseUrl}/sitemap.xml`.replace(/([^:]\/)\/+/g, "$1") // Remove double slashes
+
+    const content = `User-agent: *
+Allow: /
+
+Sitemap: ${sitemapUrl}
+`
+    await fs.promises.writeFile(path, content)
+    return [path] as FilePath[]
+  },
+  async *partialEmit() {},
+})
